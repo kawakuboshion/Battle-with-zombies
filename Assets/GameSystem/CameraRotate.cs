d95f4d6d686b211b4b140c9ea0f1c1583c997a57
sin-k <son_k0131@icloud.com>
2024-07-20 20:24:07 +0900

    敵のHPの表示を削除、カメラが後ろを見れるようにプログラム等を変更、ステージを暗くなるように変更

--- Assets/GameSystem/CameraRotate.cs
+++ Assets/GameSystem/CameraRotate.cs
@@ -4,51 +4,85 @@ using UnityEngine;
 
 public class CameraRotate : MonoBehaviour
 {
-    GameObject targetObj;
-    public float mouseXSpeed = 1;
-    public float mouseYSpeed = 1;
-    public float rotationLimitX = 0;
-    public float rotationLimitY = 0;
-    Vector3 targetPos;
-    Vector3 angle;
-    Vector3 primary_angle;
+    float x, z;
+    float speed = 0.1f;
+
+    public GameObject cam;
+    Quaternion cameraRot, characterRot;
+    float Xsensityvity = 3f, Ysensityvity = 3f;
+
+    bool cursorLock = true;
+
+    //変数の宣言(角度の制限用)
+    float minX = -90f, maxX = 90f;
+
+    // Start is called before the first frame update
     void Start()
     {
-        targetObj = GameObject.Find("Player");
-        targetPos = targetObj.transform.position;
-        angle = this.gameObject.transform.localEulerAngles;
-        primary_angle = this.gameObject.transform.localEulerAngles;
+        cameraRot = cam.transform.localRotation;
+        characterRot = transform.localRotation;
     }
 
+    // Update is called once per frame
     void Update()
     {
-        // targetの移動量分、自分（カメラ）も移動する
-        transform.position += targetObj.transform.position - targetPos;
-        targetPos = targetObj.transform.position;
-        // 回転制限
-        angle.y += Input.GetAxis("Mouse X");
-        if (angle.y <= primary_angle.y - rotationLimitX)
-        {
-            angle.y = primary_angle.y - rotationLimitX;
-        }
-        if (angle.y >= primary_angle.y + rotationLimitX)
-        {
-            angle.y = primary_angle.y + rotationLimitX;
-        }
-        angle.x -= Input.GetAxis("Mouse Y");
-        if (angle.x <= primary_angle.x - rotationLimitY)
-        {
-            angle.x = primary_angle.x - rotationLimitY;
-        }
-        if (angle.x >= primary_angle.x + rotationLimitY)
-        {
-            angle.x = primary_angle.x + rotationLimitY;
-        }
-        // targetの位置のY軸を中心に、回転（公転）する
-        this.transform.transform.localRotation = new Quaternion(0, angle.y * Time.deltaTime * mouseXSpeed, 0,0);
-        
-        // カメラの垂直移動
-        transform.RotateAround(this.transform.position, transform.right, angle.x * Time.deltaTime * mouseYSpeed);
-        this.gameObject.transform.localEulerAngles = angle;
+        float xRot = Input.GetAxis("Mouse X") * Ysensityvity;
+        float yRot = Input.GetAxis("Mouse Y") * Xsensityvity;
+
+        cameraRot *= Quaternion.Euler(-yRot, 0, 0);
+        characterRot *= Quaternion.Euler(0, xRot, 0);
+
+        //Updateの中で作成した関数を呼ぶ
+        cameraRot = ClampRotation(cameraRot);
+
+        cam.transform.localRotation = cameraRot;
+        transform.localRotation = characterRot;
+
+
+        UpdateCursorLock();
     }
+
+
+    public void UpdateCursorLock()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            cursorLock = false;
+        }
+        else if (Input.GetMouseButton(0))
+        {
+            cursorLock = true;
+        }
+
+
+        if (cursorLock)
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+        else if (!cursorLock)
+        {
+            Cursor.lockState = CursorLockMode.None;
+        }
+    }
+
+    //角度制限関数の作成
+    public Quaternion ClampRotation(Quaternion q)
+    {
+        //q = x,y,z,w (x,y,zはベクトル（量と向き）：wはスカラー（座標とは無関係の量）)
+
+        q.x /= q.w;
+        q.y /= q.w;
+        q.z /= q.w;
+        q.w = 1f;
+
+        float angleX = Mathf.Atan(q.x) * Mathf.Rad2Deg * 2f;
+
+        angleX = Mathf.Clamp(angleX, minX, maxX);
+
+        q.x = Mathf.Tan(angleX * Mathf.Deg2Rad * 0.5f);
+
+        return q;
+    }
+
+
 }
